ebf189955fce8f0fa46e86de7c23e690ff2d5291
haoliang <haoliang0.1.2@gmail.com>
2018-04-10 17:41:29 +0800

    fixup! adds ss

---
+++ shadowsocks/readme.md
@@ -0,0 +1,8 @@
+
+## what's for:
+
+dockerfile, script for running shadowsocks-server
+
+## usage:
+
+* `[password=your=password] [encrypt=aes-256-cfb] [port=6443] ./statup.sh`
--- shadowsocks/startup.sh
+++ shadowsocks/startup.sh
@@ -5,20 +5,50 @@ readonly IMAGE_NAME="ssserver"
 readonly CONTAINER_NAME="ssserver"
 
 # change it as your desire
-readonly SS_PASSWORD="123abc"
-readonly SS_ENCRYPT_METHOD="aes-256-cfb"
-readonly SS_PORT=5443
+readonly SS_PASSWORD="${password:-weiyoujingtingshan}"
+readonly SS_ENCRYPT_METHOD="${encrypt:-aes-256-cfb}"
+readonly SS_PORT="${port:-6443}"
+
+logger() {
+    >&2 printf "[%s] -- %s\n" "$(date '+%Y%m%d-%H%M%S')" "$*"
+}
 
 cd $MY_DIR || exit 1
 
-docker build -t $IMAGE_NAME .
+if [ $(docker images -q "$IMAGE_NAME:latest" | wc -l) -eq 0 ]; then
+    logger "not found $IMAGE_NAME:latest, trying to build..."
+    docker build -t $IMAGE_NAME . || {
+        logger "### build ssserver image failed"
+        exit 1
+    }
+fi
 
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
+{
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+} &>/dev/null
 
 docker run \
     -d --restart="always" \
     --name $CONTAINER_NAME \
     -p $SS_PORT:$SS_PORT \
     $IMAGE_NAME \
-    -core $(nproc) -k $SS_PASSWORD -m $SS_ENCRYPT_METHOD
+    -core $(nproc) \
+    -k $SS_PASSWORD \
+    -m $SS_ENCRYPT_METHOD \
+    -p $SS_PORT
+
+if [ $? -eq 0 ]; then
+    cat <<EOF
+#############################################################################
+# running ssserver with config:
+#############################################################################
+* server:         $(dig +short myip.opendns.com @resolver1.opendns.com)
+* port:           $SS_PORT
+* password:       $SS_PASSWORD
+* encrypt-method: $SS_ENCRYPT_METHOD
+EOF
+else
+    logger "failed to start ssserver"
+    exit 1
+fi
